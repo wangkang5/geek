81497ddcf05cc12b42224b436ffb07eb111531f0
wangkang <11140612@vivo.com>
2022-03-30 19:12:35 +0800

    m

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,7 @@ module.exports = {
   title: 'Hello MM',
   theme: '@vuepress/theme-blog',
   description: 'Just playing around',
+  base: "/geek/",
   configureWebpack: {
     resolve: {
       alias: {
